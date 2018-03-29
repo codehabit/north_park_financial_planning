fefb66e2027df92f58fec4c6312eabd7abef583d
David Oliver <david.oliver@codehabit.net>
2018-03-29 07:37:44 -0600

    Adds initial about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :accounts, only: [:new, :create, :show]
+  get "/about", to: "about#index", as: :about
   root to: "home#index", as: :home
 end
--- spec/features/top_nav_spec.rb
+++ spec/features/top_nav_spec.rb
@@ -7,7 +7,13 @@ RSpec.describe HomeController do
     expect(current_path).to eq new_account_path
   end
 
-  it "can visit the home page" do
+  it "can visit the about us page" do
+    visit home_path
+    click_on "learn more about us"
+    expect(current_path).to eq about_path
+  end
+
+  it "can get back to the home page" do
     visit home_path
     click_on "get started"
     expect(current_path).to eq new_account_path
